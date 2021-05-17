de870bf970fa902b1111c2fd7f197b1419b8683a
martijnwijs <martijnwijsman@gmail.com>
2021-05-17 12:06:40 +0200

    (testing , print statements)

--- ass2/lambdaMART/xgboost.py
+++ ass2/lambdaMART/xgboost.py
@@ -7,7 +7,7 @@ import warnings
 
 warnings.filterwarnings(action='ignore', category=UserWarning)
 
-def Groupshufflesplit(df, size=None):
+def Groupshufflesplit(df, size=None, features):
     '''splits the training dataset in a train + validation dataset, if argument size is given, only a part of the dataset is used to train faster
     groups give the indices of the different srch id's'''
     if size is not None: # use only part of dataset
@@ -28,40 +28,102 @@ def Groupshufflesplit(df, size=None):
 
 
     # remove features
+    X_train = X_train[feature]
+    X_val = X_val[features]
+
     return X_train, y_train, X_val, y_val, groups
+
+
+# global variables
+file_name = "xbg.pkl"
+
+features = ['srch_id', 'site_id', 'visitor_location_country_id',
+    'visitor_hist_starrating', 'visitor_hist_adr_usd', 'prop_country_id',
+    'prop_id', 'prop_starrating', 'prop_review_score', 'prop_brand_bool',
+    'prop_location_score1', 'prop_location_score2',
+    'prop_log_historical_price', 'position', 'price_usd', 'promotion_flag',
+    'srch_destination_id', 'srch_length_of_stay', 'srch_booking_window',
+    'srch_adults_count', 'srch_children_count', 'srch_room_count',
+    'srch_saturday_night_bool', 'srch_query_affinity_score',
+    'orig_destination_distance', 'random_bool', 'comp1_rate', 'comp1_inv',
+    'comp1_rate_percent_diff', 'comp2_rate', 'comp2_inv',
+    'comp2_rate_percent_diff', 'comp3_rate', 'comp3_inv',
+    'comp3_rate_percent_diff', 'comp4_rate', 'comp4_inv',
+    'comp4_rate_percent_diff', 'comp5_rate', 'comp5_inv',
+    'comp5_rate_percent_diff', 'comp6_rate', 'comp6_inv',
+    'comp6_rate_percent_diff', 'comp7_rate', 'comp7_inv',
+    'comp7_rate_percent_diff', 'comp8_rate', 'comp8_inv',
+    'comp8_rate_percent_diff']
 
 
 if __name__ == "main":
 
-    # import dataset
-    df = pd.read_csv("training_set_VU_DM.csv")
+    # train the model
+    if training == True:
+        # import dataset
+        df = pd.read_csv("training_set_VU_DM.csv")
+        print("dataset loaded")
 
-    # XBG initialization
-    model = xgb.XGBRanker(  
-        objective='rank:pairwise',
-        random_state=42, 
-        learning_rate=0.1,
-        colsample_bytree=0.9, 
-        eta=0.05, 
-        max_depth=6, 
-        n_estimators=110, 
-        subsample=0.75 
-        )
+        # XBG initialization
+        model = xgb.XGBRanker(  
+            objective='rank:pairwise',
+            random_state=42, 
+            learning_rate=0.1,
+            colsample_bytree=0.9, 
+            eta=0.05, 
+            max_depth=6, 
+            n_estimators=110, 
+            subsample=0.75 
+            )
 
-    # add scores to dataframe
-    df["scores"] = df.apply (lambda row: add_scores(row), axis=1) 
-    
-    X_train, y_train, X_val, y_val, groups = Groupshufflesplit(df, 100000) # change this value to get different sizes
-    model.fit(X_train, y_train, group=groups, verbose=True) # train the model
+        # add scores to dataframe
+        df["scores"] = df.apply (lambda row: add_scores(row), axis=1) 
+        
+        # extract relevant features from dataset
 
-    # validate the model
-    y_val_model = model.predict(X_validate)
+        X_train, y_train, X_val, y_val, groups = Groupshufflesplit(df, 100000, features) # change this value to get different sizes
 
-    #insert to X_val
-    X_val["scores"] = y_val_model
+        print("data prepared, starting training...")
+        model.fit(X_train, y_train, group=groups, verbose=True) # train the model
+        print("model trained")
 
-    # calculate ndcg
-    evaluate_score(X_val, y_val):
+        # validate the model
+        y_val_predict = model.predict(X_val)
 
-    # rank the testset
-    #rank_variable(output, "scores")
+        # calculate ndcg
+        val_ndcg = evaluate_score(y_val_predict, y_val, groups):
+        print("validation_ndcg: ", val_ndcg)
+        
+        # save
+        pickle.dump(model, open(file_name, "wb"))
+        print("model saved")
+        print("finished")
+
+    # evaluate on testset
+    if testing==True:
+
+        # load model
+        model = pickle.load(open(file_name, "rb"))
+        print("xgboost loaded")
+
+        # import dataset
+        df_test = pd.read_csv("test_set_VU_DM.csv")
+        print("dataset loaded")
+        X_test = df_test[features]
+
+        # predict
+        y_test_predict = model.predict(X_test)
+        print("predicted y values!")
+
+        # create output df
+        output = df_test[["srch_id", "prop_id"]]
+        output["scores"] = y_test_predict
+
+        # rank on score
+        output_ranked = rank_variable(output, "scores")
+        print("rearranged on scores per search query")
+
+        # output to csv file
+        pandas_to_csv(output_ranked, filename="out3.csv")
+        print("saved as csv file")
+        print("finished")
